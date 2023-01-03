9fee61efa86e07f663dda55fab74b57407a037ff
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-01-02 23:14:13 -0600

    javascript exercise 4

---
+++ exercise4.js
@@ -0,0 +1,10 @@
+//* Programa una función que repita un texto X veces, ejemplo. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo
+
+function repeaText(text) {
+    let result = text.repeat(3);
+
+    return result;
+
+}
+
+console.log(repeaText('Hello World, '));
